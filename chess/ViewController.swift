72d97591e71a172db34df6a3313b4a63014ad691
Elena Khaletska <yeliena.khaletska@pipedrive.com>
2024-06-21 10:16:19 +0300

    Create empty Swift project

---
+++ chess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chess
+//
+//  Created by Yeliena Khaletska on 21.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
