95caa8271758773980c749e0a26e16f0f534be73
Elena Khaletska <yeliena.khaletska@pipedrive.com>
2024-07-08 13:45:58 +0300

    Draw full screen board in portrait mode

---
+++ chess/chess/ChessPiece.swift
@@ -0,0 +1,8 @@
+//
+//  Piece.swift
+//  chess
+//
+//  Created by Yeliena Khaletska on 08.07.2024.
+//
+
+import Foundation
--- chess/chess/ContentView.swift
+++ chess/chess/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ChessBoard()
+    }
+}
+
+struct ChessBoard: View {
+    let columns = Array(repeating: GridItem(.flexible(), spacing: 0), count: 8)
+
+    var body: some View {
+        LazyVGrid(columns: columns, spacing: 0) {
+            ForEach(0..<64) { index in
+                Rectangle()
+                    .foregroundColor(colorForSquare(at: index))
+                    .aspectRatio(1, contentMode: .fit)
+            }
         }
-        .padding()
+    }
+
+    func colorForSquare(at index: Int) -> Color {
+        let row = index / 8
+        let column = index % 8
+        return (row + column).isMultiple(of: 2) ? .white : .black
     }
 }
 
