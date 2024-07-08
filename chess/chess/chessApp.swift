2632e70f001529111d9bc6a065e0b316085f5824
Elena Khaletska <yeliena.khaletska@pipedrive.com>
2024-07-08 13:45:42 +0300

    Restart with SwiftUI

---
+++ chess/chess/chessApp.swift
@@ -0,0 +1,17 @@
+//
+//  chessApp.swift
+//  chess
+//
+//  Created by Yeliena Khaletska on 08.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct chessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
