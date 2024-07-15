29f064c47f75c5be645b074da630867dab91d94a
Andrius Shiaulis <shiaulis@gmail.com>
2024-07-15 11:45:12 +0300

    5: Implement Pawn promotion logic (#19)
    * 5: Implement Pawn promotion logic
    
    * PR fixes

--- chess/UI/ChessGameView.swift
+++ chess/UI/ChessGameView.swift
@@ -17,10 +17,39 @@ struct ChessGameView: View {
             .onAppear {
                 self.chessViewModel.gameAppeared()
             }
+            .errorAlert(error: self.$chessViewModel.error)
     }
 
 }
 
 #Preview {
     ChessGameView(chessViewModel: ChessGameViewModel())
+}
+
+extension View {
+    func errorAlert(error: Binding<Error?>, buttonTitle: String = "OK") -> some View {
+        let localizedAlertError = LocalizedAlertError(error: error.wrappedValue)
+        return alert(isPresented: .constant(localizedAlertError != nil), error: localizedAlertError) { _ in
+            Button(buttonTitle) {
+                error.wrappedValue = nil
+            }
+        } message: { error in
+            Text(error.recoverySuggestion ?? "")
+        }
+    }
+}
+
+struct LocalizedAlertError: LocalizedError {
+    let underlyingError: LocalizedError
+    var errorDescription: String? {
+        underlyingError.errorDescription
+    }
+    var recoverySuggestion: String? {
+        underlyingError.recoverySuggestion
+    }
+
+    init?(error: Error?) {
+        guard let localizedError = error as? LocalizedError else { return nil }
+        underlyingError = localizedError
+    }
 }
